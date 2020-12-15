76e730df41b50fbb157006fa079327acec989375
slevaisprava <slevaisprava@gmail.com>
2020-12-16 00:08:36 +0300

    Revert env-functions with linspace.

--- ftgens.py
+++ ftgens.py
@@ -7,8 +7,8 @@ from numba.typed import List
 cc = CC('my_module')
 
 
-@cc.export('line_line', 'f8[:](f8[:], i4[:], f8[:])')
-def line_line(values, times, curves):
+@cc.export('env', 'f8[:](f8[:], i4[:], f8[:])')
+def env(values, times, curves):
     times = _justify_arrays(values, times)
     curves = _justify_arrays(values, curves)
 
@@ -54,51 +54,56 @@ def _justify_arrays(first, second):
         second = np.append(second, second[-1])
     return second
 
-#@cc.export('line_curve', 'f8[:](f8[:], i4[:], f8[:])')
-#def line_curve(values, times, curves):
-#    times = _justify_arrays(values, times)
-#    curves = _justify_arrays(values, curves)
-#    table = List()
-#    i = 0
-#    for time in times:
-#        arr = np.linspace(values[i], values[i+1], time)
-#        if curves[i] != 0:
-#            arr = make_curves(arr, curves[i])
-#        table.append(arr)
-#        i += 1
-#    return _flate(table)
-#@njit
-#def make_curves(arr, curve):
-#    mn = np.min(arr)
-#    mx = np.max(arr)
-#    if mn != mx:
-#        grow = np.exp(curve)
-#        a = (mx - mn)/(1.0 - grow)
-#        b = mn + a;
-#        arr = b - (a * np.power(grow, (arr - mn)/(mx - mn)))
-#    return arr
-#@njit
-#def _flate(table):
-#    res = np.empty(_calc_res_size(table))
-#    i = 0
-#    k = 0
-#    for arr in table:
-#        j = 0
-#        for val in arr:
-#            if k == 0 or j != 0:
-#                res[i] = val
-#                i += 1
-#            j += 1
-#        k += 1
-#    return res
-#
-#
-#@njit
-#def _calc_res_size(table):
-#    res_size = 0
-#    for arr in table:
-#        res_size += arr.size
-#    return res_size - (len(table)-1)
+
+@cc.export('cycle_env', 'f8[:](f8[:], i4[:], f8[:])')
+def cycle_env(values, times, curves):
+    times = _justify_arrays(values, times)
+    curves = _justify_arrays(values, curves)
+    table = List()
+    i = 0
+    for time in times:
+        arr = np.linspace(values[i], values[i+1], time)
+        if curves[i] != 0:
+            arr = make_curves(arr, curves[i])
+        table.append(arr)
+        i += 1
+    return _flate(table)
+
+
+@njit
+def make_curves(arr, curve):
+    mn = np.min(arr)
+    mx = np.max(arr)
+    if mn != mx:
+        grow = np.exp(curve)
+        a = (mx - mn)/(1.0 - grow)
+        b = mn + a;
+        arr = b - (a * np.power(grow, (arr - mn)/(mx - mn)))
+    return arr
+@njit
+
+
+def _flate(table):
+    res = np.empty(_calc_res_size(table))
+    i = 0
+    k = 0
+    for arr in table:
+        j = 0
+        for val in arr:
+            if k == 0 or j != 0:
+                res[i] = val
+                i += 1
+            j += 1
+        k += 1
+    return res
+
+
+@njit
+def _calc_res_size(table):
+    res_size = 0
+    for arr in table:
+        res_size += arr.size
+    return res_size - (len(table)-1)
 
 
 if __name__ == "__main__":
--- ftgens_wrapper.py
+++ ftgens_wrapper.py
@@ -1,30 +1,31 @@
 import time
 import gc
-
 import numpy as np
 import matplotlib.pyplot as plt
 import my_module
 
-def line_curve(values, times, curves):
-    values = np.array(values, dtype=np.float)
-    times = np.array(times, dtype=np.int32)
-    curves = np.array(curves, dtype=np.float)
-    return my_module.line_curve(values, times, curves)
 
-def line(values, times, curves):
+def env(values, times, curves):
     values = np.array(values, dtype=np.float)
     times = np.array(times, dtype=np.int32)
     curves = np.array(curves, dtype=np.float)
-    return my_module.line_line(values, times, curves)
+    return my_module.env(values, times, curves)
+
+def cycle_env1(values, times, curves):
+    values = np.array(values, dtype=np.float)
+    times = np.array(times, dtype=np.int32)
+    curves = np.array(curves, dtype=np.float)
+    return my_module.cycle_env(values, times, curves)
 
 #t1 = time.time()
 #a = line_curve([0, 12,0], [1000000], [5])
 #print(time.time()-t1)
 
 t1 = time.time()
-b = line([0, 12, 0], [1000000], [5])
-print(time.time()-t1, np.max(b))
+b = cycle_env1([5, 12, 0]*5, [1000], [5,5]*6)
+#print(time.time()-t1, np.max(b))
 
 plt.plot(b)
 plt.show()
+gc.collect()
 #

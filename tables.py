77ac956845df1fcc333d0d0db8c2414298bec472
slevaisprava <slevaisprava@gmail.com>
2020-12-17 06:58:16 +0300

    Add prohibiting the use of identical tables.

--- tables.py
+++ tables.py
@@ -12,9 +12,9 @@ class ParseTables:
         self.src = src
         self.tab_def = None
         self.tab_name = None
-
         self.tab_num = 0
-        self.table_records = set()
+
+        self.table_records = dict()
         self.ftgens = []
 
     def replace_table_definitions(self):
@@ -23,19 +23,23 @@ class ParseTables:
 
     def _table_proc(self, obj: re.Match):
         self.tab_num += 1
-        self.tab_def = obj.groups()
-        self.tab_def = [self.re_white_space.sub('', s) for s in self.tab_def]
+        self.tab_def = [self.re_white_space.sub('', s) for s in obj.groups()]
         self.tab_name = f'gi_tab_{self.orc_num}_{self.tab_num}'
         self._make_table_record()
         return self.tab_name
 
     def _make_table_record(self):
-        hash_dig = self.make_hash_dig()
-        self.tab_def.append(hash_dig)
-        self.table_records.add(tuple(self.tab_def))
-        self.ftgens.append(self.tab_name + ' ftgen ' + hash_dig)
+        hash_dig = self._make_hash_dig()
+        self.tab_def.append(self.tab_name)
 
-    def make_hash_dig(self):
+        if hash_dig in self.table_records:
+            existing_name = self.table_records[hash_dig][4]
+            self.ftgens.append(self.tab_name + ' = ' + existing_name)
+        else:
+            self.table_records[hash_dig] = self.tab_def
+            self.ftgens.append(self.tab_name + ' ftgen ' + hash_dig)
+
+    def _make_hash_dig(self):
         hash_object = hashlib.sha1(str(self.tab_def).encode())
         hex_dig = hash_object.hexdigest()
         return hex_dig[:20]
@@ -50,7 +54,8 @@ if __name__ == "__main__":
     SRC = '''
         table(~[123,23], [11,17], [5], 12, 78)
         table([123,23], [11, 16], [5], 12, 78)
-        table([123,23], [11,16], [5], 12, 78)
+        table([123,23], [ 11 ,16], [ 5 ], 12, 78)
+        table([123,23], [ 11 ,16], [ 5 ], 12, 78)
     '''
     t = ParseTables(SRC, 1)
     s, f = t.replace_table_definitions()

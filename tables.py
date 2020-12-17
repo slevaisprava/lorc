0f6b96ba69676e85d7a77f540527d0e2cc84f224
slevaisprava <slevaisprava@gmail.com>
2020-12-17 06:30:16 +0300

    Add removing white spaces for hashing.

--- tables.py
+++ tables.py
@@ -5,6 +5,7 @@ import hashlib
 class ParseTables:
     lst = '(\\[.*?\\]\\s*(?:\\*\\s*\\d*)?)'
     re_table_data = re.compile(f'([~]?)\\s*{lst}\\s*,\\s*{lst}\\s*,\\s*{lst}')
+    re_white_space = re.compile('\\s+')
 
     def __init__(self, src, orc_num):
         self.orc_num = orc_num
@@ -23,12 +24,13 @@ class ParseTables:
     def _table_proc(self, obj: re.Match):
         self.tab_num += 1
         self.tab_def = obj.groups()
+        self.tab_def = [self.re_white_space.sub('', s) for s in self.tab_def]
         self.tab_name = f'gi_tab_{self.orc_num}_{self.tab_num}'
         self._make_table_record()
+        return self.tab_name
 
     def _make_table_record(self):
         hash_dig = self.make_hash_dig()
-        self.tab_def = list(self.tab_def)
         self.tab_def.append(hash_dig)
         self.table_records.add(tuple(self.tab_def))
         self.ftgens.append(self.tab_name + ' ftgen ' + hash_dig)
@@ -38,6 +40,7 @@ class ParseTables:
         hex_dig = hash_object.hexdigest()
         return hex_dig[:20]
 
+
 class MakeTables:
     def __init__(self, data):
         pass
@@ -46,7 +49,7 @@ class MakeTables:
 if __name__ == "__main__":
     SRC = '''
         table(~[123,23], [11,17], [5], 12, 78)
-        table([123,23], [11,16], [5], 12, 78)
+        table([123,23], [11, 16], [5], 12, 78)
         table([123,23], [11,16], [5], 12, 78)
     '''
     t = ParseTables(SRC, 1)

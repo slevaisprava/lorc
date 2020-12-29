e00dad65827afdb33061418faf6d9ad5e202d5df
slevaisprava <slevaisprava@gmail.com>
2020-12-29 03:26:21 +0300

    LRU: Rename private methods.

--- lru_files.py
+++ lru_files.py
@@ -8,7 +8,7 @@ CAPACITY = 20
 class LRUFiles:
     def __init__(self, path):
         self._path = self.__class__.make_path(path)
-        self._cache = self.make_cache()
+        self._cache = self._make_cache()
 
     @property
     def path(self):
@@ -21,7 +21,7 @@ class LRUFiles:
             os.makedirs(path)
         return path
 
-    def make_cache(self):
+    def _make_cache(self):
         files = os.listdir(self._path)
         return OrderedDict.fromkeys(files)
 
@@ -34,9 +34,9 @@ class LRUFiles:
     def put(self, hex_dig):
         self._cache[hex_dig] = None
         self._cache.move_to_end(hex_dig)
-        self.clear_cache()
+        self._clear_cache()
 
-    def clear_cache(self):
+    def _clear_cache(self):
         cache_len = len(self._cache)
         if cache_len > CAPACITY:
             for _ in range(cache_len-CAPACITY):

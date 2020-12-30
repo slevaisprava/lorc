35696817e9d0fa910584acf93eb111a11a30ae7a
slevaisprava <slevaisprava@gmail.com>
2020-12-30 12:31:41 +0300

    Vim: Add start_single_orc function.

--- vim/startup.py
+++ vim/startup.py
@@ -9,6 +9,16 @@ from lorc.gens import my_module
 from lorc.vim  import startup
 
 
+def load_vim_scripts():
+    path = os.path.realpath(__file__)
+    path = os.path.dirname(path)
+    vim_conf_file = os.path.join(path, 'startup.vim')
+
+    with open(vim_conf_file, 'r') as f:
+        vim_script = f.read()
+        vim.command(vim_script)
+
+
 def reload_modules():
     importlib.reload(startup)
     importlib.reload(my_module)
@@ -20,21 +30,19 @@ def reload_modules():
     print('Done')
 
 
-path = os.path.realpath(__file__)
-path = os.path.dirname(path)
-vim_conf_file = os.path.join(path, 'startup.vim')
-
-with open(vim_conf_file, 'r') as f:
-    vim_script = f.read()
-    vim.command(vim_script)
-
-
-def pr():
+def start_single_orc(b):
     src = '\n'.join(vim.current.buffer[:])
     orch = orchestra.Orchestra(src, 1)
+
     with open('/dev/shm/sample.orc', 'w') as f:
         f.writelines(orch.orchestra)
-    vim.command('call Term_Start()')
+
+    cmd = vim.current.buffer[0].lstrip(';').split()
+    vim.command(f'call Term_Start({cmd})')
+
 
 def pr1():
     envelopes.ParseEnvelope.cache._clear_cache()
+
+
+load_vim_scripts()
--- vim/startup.vim
+++ vim/startup.vim
@@ -2,11 +2,11 @@ function! Term_Exit(job, ec)
 	py3 startup.pr1()
 endfunction	
 
-function! Term_Start()
-	rightbelow call term_start( "csound -m0d -odac --orc /dev/shm/sample.orc", { "vertical": 0, "term_name": "csound_terminal", "exit_cb": "Term_Exit" })
+function! Term_Start(cmd)
+	rightbelow call term_start(a:cmd , { "vertical": 0, "term_name": "csound_terminal", "exit_cb": "Term_Exit"})
 	tmap <silent><buffer> <M-j> <c-w>:bw!<cr>
 	map  <silent><buffer> <M-j> :bw!<cr>
 endfunction
 
-nmap <silent> <M-j> :silent py3 startup.pr()<cr>
+nmap <silent> <M-j> :py3 startup.start_single_orc(vim.eval('bufname("%")'))<cr>
 nmap <silent> <M-r> :py3 startup.reload_modules()<cr>
